7e30a31677a4924c114b28e866a5979110d3a3c7
stevenpy <sphilbert@ventadis.fr>
2019-01-29 13:25:31 +0100

    add docker-compose.yml

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            AAACCRFGHJYT
+            AAA
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
